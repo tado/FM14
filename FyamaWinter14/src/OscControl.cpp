23f779ad29a929ed4d1acff5ef3badd929e661fd
Atsushi Tadokoro <tadokoro@gmail.com>
2014-11-18 08:31:06 +0900

    update

--- FyamaWinter14/src/OscControl.cpp
+++ FyamaWinter14/src/OscControl.cpp
@@ -1,6 +1,15 @@
 #include "OscControl.h"
 #include "ofApp.h"
 
+
+string stateMatrix[5][8] = {
+    {"StFftDrawCircle", "StFftBox", "StFftRibbon"},
+    {"StFftDrawBar", "StFftSphereStripe", "StFftDistort", "StFftRibbon"},
+    {"StSoundWave", "StFftDistortStripe", "StFftSphereStripe", "StFftSphereDistort", "StKaleidoscope"},
+    {"StSimplePixelate", "StTrianglePixelate", "StCvOpDraw", "StCvOpParticle", "StCvOpParticlePath", "StCvOpNote", "StCvOpMesh", "StCvOpDistort"},
+    {"StRgbDelay", "StFftDot", "StSakuraParticle", "", "", "", "", "StBlank"}
+};
+
 void OscControl::setup(){
     receiver.setup(9000);
     for (int i = 0; i < 8; i++) {
@@ -19,73 +28,84 @@ void OscControl::update(){
             
             int row = m.getArgAsInt32(0);
             int col = m.getArgAsInt32(1);
+
+            for (int j = 0; j < 8; j++) {
+                for (int i = 0; i < 5; i++) {
+                    if ((i == row - 53) && (j == col - 1) && stateMatrix[i][j].length() > 0) {
+                        //cout << "Select State : " << stateMatrix[i][j] << endl;
+                        app->stateMachine.changeState(stateMatrix[i][j]);
+                    }
+                }
+            }
             
+            /*
             if (row == 53 && col == 1) {
-                app->stateMachine.changeState("StBlank");
+                app->stateMachine.changeState("StBlank"); x
             }
             if (row == 53 && col == 2) {
-                app->stateMachine.changeState("StSimplePixelate");
+                app->stateMachine.changeState("StSimplePixelate"); x
             }
             if (row == 53 && col == 3) {
-                app->stateMachine.changeState("StCvOpDraw");
+                app->stateMachine.changeState("StCvOpDraw"); x
             }
             if (row == 53 && col == 4) {
-                app->stateMachine.changeState("StCvOpParticle");
+                app->stateMachine.changeState("StCvOpParticle"); x
             }
             if (row == 53 && col == 5) {
-                app->stateMachine.changeState("StCvOpParticlePath");
+                app->stateMachine.changeState("StCvOpParticlePath"); x
             }
             if (row == 53 && col == 6) {
-                app->stateMachine.changeState("StCvOpMesh");
+                app->stateMachine.changeState("StCvOpMesh"); x
             }
             if (row == 53 && col == 7) {
-                app->stateMachine.changeState("StCvOpNote");
+                app->stateMachine.changeState("StCvOpNote"); x
             }
             if (row == 53 && col == 8) {
-                app->stateMachine.changeState("StCvOpDistort");
+                app->stateMachine.changeState("StCvOpDistort"); x
             }
             if (row == 54 && col == 1) {
-                app->stateMachine.changeState("StSakuraParticle");
+                app->stateMachine.changeState("StSakuraParticle"); x
             }
             if (row == 54 && col == 2) {
-                app->stateMachine.changeState("StRgbDelay");
+                app->stateMachine.changeState("StRgbDelay"); x
             }
             if (row == 54 && col == 3) {
                 app->stateMachine.changeState("StTrianglePixelate");
             }
             if (row == 55 && col == 1) {
-                app->stateMachine.changeState("StSoundWave");
+                app->stateMachine.changeState("StSoundWave"); x
             }
             if (row == 55 && col == 2) {
-                app->stateMachine.changeState("StFftDrawCircle");
+                app->stateMachine.changeState("StFftDrawCircle"); x
             }
             if (row == 55 && col == 3) {
                 app->stateMachine.changeState("StFftDot");
             }
             if (row == 55 && col == 4) {
-                app->stateMachine.changeState("StFftDrawBar");
+                app->stateMachine.changeState("StFftDrawBar"); x
             }
             if (row == 55 && col == 5) {
-                app->stateMachine.changeState("StFftBox");
+                app->stateMachine.changeState("StFftBox"); x
             }
             if (row == 55 && col == 6) {
-                app->stateMachine.changeState("StFftRibbon");
+                app->stateMachine.changeState("StFftRibbon"); x
             }
             if (row == 55 && col == 7) {
-                app->stateMachine.changeState("StFftDistort");
+                app->stateMachine.changeState("StFftDistort"); x
             }
             if (row == 55 && col == 8) {
-                app->stateMachine.changeState("StFftDistortStripe");
+                app->stateMachine.changeState("StFftDistortStripe"); x
             }
             if (row == 56 && col == 1) {
-                app->stateMachine.changeState("StFftSphereStripe");
+                app->stateMachine.changeState("StFftSphereStripe"); x
             }
             if (row == 56 && col == 2) {
-                app->stateMachine.changeState("StFftSphereDistort");
+                app->stateMachine.changeState("StFftSphereDistort"); x
             }
             if (row == 56 && col == 3) {
-                app->stateMachine.changeState("StKaleidoscope");
+                app->stateMachine.changeState("StKaleidoscope"); x
             }
+             */
         }
         
         if(m.getAddress() == "/control"){
