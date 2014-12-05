2f805ced6145d3f7c6caa3b721495006f0500ec9
Atsushi Tadokoro <tadokoro@gmail.com>
2014-12-05 18:24:06 +0900

    update

--- FyamaWinter14/src/AlphaMask.cpp
+++ FyamaWinter14/src/AlphaMask.cpp
@@ -13,15 +13,16 @@ AlphaMask::AlphaMask(){
     maskShader.load("composite");
     maskShader.begin();
     
-    maskShader.setUniformTexture("Tex1", mask.getTextureReference(), 1);
-    maskShader.end();
-    
     ofApp *app = ((ofApp*)ofGetAppPtr());
     srcTex = app->blackmagic->colorTexture;
+    
+    maskShader.setUniformTexture("Tex1", mask.getTextureReference(), 1);
+    maskShader.end();
 }
 
 void AlphaMask::update(){
     ofApp *app = ((ofApp*)ofGetAppPtr());
+    
     srcTex = app->blackmagic->colorTexture;
     maskShader.setUniformTexture("Tex0", srcTex, 0);
 }
@@ -31,6 +32,7 @@ void AlphaMask::draw(){
     srcTex = app->blackmagic->colorTexture;
 
     fbo.begin();
+    ofClear(0, 0, 0);
     maskShader.begin();
     
     glActiveTexture(GL_TEXTURE0_ARB);
@@ -42,24 +44,26 @@ void AlphaMask::draw(){
     
     glBegin(GL_QUADS);
     
-    float maskOffset = 0;
+    float maskOffsetX =  ofMap(app->oscControl->controlVal[5], 0, 127, -mask.getWidth(), mask.getWidth() / 3.0);
+    float maskOffsetY =  ofMap(app->oscControl->controlVal[5], 0, 127, -mask.getHeight(), mask.getHeight() / 3.0);
+    
     glMultiTexCoord2d(GL_TEXTURE0_ARB, 0, 0);
-    glMultiTexCoord2d(GL_TEXTURE1_ARB, 0, maskOffset);
-    glVertex2f( 0, 0);
+    glMultiTexCoord2d(GL_TEXTURE1_ARB, maskOffsetX, maskOffsetY);
+    glVertex2f(0, 0);
     
     // glMultiTexCoord2d(GL_TEXTURE0_ARB, topLayer.getWidth(), 0);
     glMultiTexCoord2d(GL_TEXTURE0_ARB, srcTex.getWidth(), 0);
-    glMultiTexCoord2d(GL_TEXTURE1_ARB, mask.getWidth(), 0);
-    glVertex2f( ofGetWidth(), 0);
+    glMultiTexCoord2d(GL_TEXTURE1_ARB, mask.getWidth() - maskOffsetX, maskOffsetY);
+    glVertex2f(ofGetWidth(), 0);
     
     // glMultiTexCoord2d(GL_TEXTURE0_ARB, topLayer.getWidth(), topLayer.getHeight());
     glMultiTexCoord2d(GL_TEXTURE0_ARB, srcTex.getWidth(), srcTex.getHeight());
-    glMultiTexCoord2d(GL_TEXTURE1_ARB, mask.getWidth(), mask.getHeight());
+    glMultiTexCoord2d(GL_TEXTURE1_ARB, mask.getWidth() - maskOffsetX, mask.getHeight() - maskOffsetY);
     glVertex2f( ofGetWidth(), ofGetHeight());
     
     // glMultiTexCoord2d(GL_TEXTURE0_ARB, 0, topLayer.getHeight());
     glMultiTexCoord2d(GL_TEXTURE0_ARB, 0, srcTex.getHeight());
-    glMultiTexCoord2d(GL_TEXTURE1_ARB, 0, mask.getHeight());
+    glMultiTexCoord2d(GL_TEXTURE1_ARB, maskOffsetX, mask.getHeight() - maskOffsetY);
     glVertex2f(0, ofGetHeight());
     
     glEnd();
@@ -81,7 +85,7 @@ void AlphaMask::draw(){
     ofPushMatrix();
     ofRect(0, 0, 1920, 224);
     ofTranslate(0, 630 + 224);
-    ofRect(0, 0, 1920, 224);
+    ofRect(0, 0, 1920, 300);
     ofPopMatrix();
 
 }
